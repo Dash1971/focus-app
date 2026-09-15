91a144adcaa3cab536c38eaef1034d915fa5c2b3
Dash1971 <277174790+Dash1971@users.noreply.github.com>
2026-09-15 14:45:14 +0900

    Prepare LockIn 0.4.0 build 6

--- scripts/generate_xcodeproj.rb
+++ scripts/generate_xcodeproj.rb
@@ -19,7 +19,7 @@ def configure(target, bundle_id:, plist: nil, entitlements: nil)
     settings['TARGETED_DEVICE_FAMILY'] = '1'
     settings['SWIFT_VERSION'] = '5.0'
     settings['CODE_SIGN_STYLE'] = 'Automatic'
-    settings['CURRENT_PROJECT_VERSION'] = '5'
+    settings['CURRENT_PROJECT_VERSION'] = '6'
     settings['MARKETING_VERSION'] = '0.4.0'
     settings['CODE_SIGN_ENTITLEMENTS'] = entitlements if entitlements
     if plist
